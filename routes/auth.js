acc513fdd863035ec8ccc11c156163f9b090235c
Gurveer2711 <singhgurveer337@gmail.com>
2025-01-03 13:13:21 +0530

    corrected the login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
 const User = require("../models/user");
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //register router
 router.post("/register", async (req, res) => {
@@ -13,40 +13,42 @@ router.post("/register", async (req, res) => {
       return res
         .status(400)
         .json({ message: "User already exits with this email" });
-      }
+    }
     const newUser = new User({
       name: name,
       email: email,
       password: password,
-    });  
-      await newUser.save();
-      res.json("Account has been created");
+    });
+    const token = await newUser.save();
+    
   } catch (error) {
     res.status(500).json({ message: "Server error" });
-    }
+  }
 });
 
 //login router
-router.post('/login', async (req, res) => {
-    try {
-
-        const { email, password } = req.body;
-        if (!email || !password) {
-            res.status(401).json({ error: "Email and password are required" });
-        }
-        const user = await User.findOne({ email: email });
-        if (!user) {
-          return res.status(401).json({ error: "Invalid email or password" });
-        }
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-          return res.status(401).json({ error: "Invalid email or password" });
-        }
-        res.status(200).json({ message: 'Login successful' });
-    } catch (error) {
-        console.error("Error during login:", error);
-        res.status(500).json({ error: "Internal server error" });
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(401).json({ error: "Email and password are required" });
     }
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: "1d",});
+    req.session.user = { token };
+
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
-module.exports = router;    
+module.exports = router;
